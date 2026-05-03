a6046b11b781e1635bbcfe5b85ef37c99856cdeb
Maketra17 <alicetofsa@mail.ru>
2026-05-03 18:09:04 +0500

    first commit

---
+++ app/src/main/java/com/bignerdranch/android/myapplicatio/LaunchActivity.kt
@@ -0,0 +1,24 @@
+package com.bignerdranch.android.myapplicatio
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+
+class LaunchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val timer = object: CountDownTimer(3000,1000) {
+            override fun onTick(millisUntilFinished: Long) {
+            }
+
+            override fun onFinish() {
+                val intent = Intent(this@LaunchActivity, LaunchActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+        timer.start()
+    }
+}
